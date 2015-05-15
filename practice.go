75c7b6c813cc222bec0360f51b7dc00d152c9f1b
Daniel A. Noventa <daniel.a.noventa@gmail.com>
2015-05-14 21:08:15 -0500

    Functions and importing.

--- practice.go
+++ practice.go
@@ -1,10 +1,18 @@
 package main
 
+// factored import statements. 
 import (
 	"fmt"
 	"math/rand"
+	"math"
 )
+
+func add(x int, y int) int {
+	return x + y
+}
 
 func main() {
 	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println(math.Pi)
+	fmt.Println(add(42, 13))
 }
